d8103520b8af589d9c37a1979c3c0eebe1f8e237
sspboy <sspboy@163.com>
2024-06-28 18:00:39 +0800

    20240628

--- openvc/main.py
+++ openvc/main.py
@@ -1,8 +1,10 @@
 import numpy as np
 import cv2,os
+import numpy as np
 # 2D图片处理库
 from matplotlib import pyplot as plt
 
+# 匹配模版图片方法；返回截图
 class Selectimg():
 
     def __init__(self,img_url,template_img_url):
@@ -60,29 +62,32 @@ class Selectimg():
 
         lr = bottomRight[0] # 左边到模版图片右边的距离
 
-        # 匹配图片模版位置=====结束
-        img1 = self.detaile_img[tt:tb,ll:lr]
+        img1 = self.detaile_img[tt:tb,ll:lr]    # 根据边距剪切匹配的
 
-        # save_path = r'./img'                                      # 存储路径配置
-        # save_path_file = os.path.join(save_path, 'heheda.jpg')    # 存储图片到本地文件夹
-        # cv2.imwrite(save_path_file, img1)                         # 存储
-        #
-        # # 预览截取的图片
-        # hehda = cv2.imread('img/heheda.jpg')
         cv2.imshow('image', img1)
         cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        cv2.destroyWindow('image')
 
+    # 保存图片到本地
+    def save_img(self, img, name):
+        save_path = r'./img'                                      # 存储路径配置
+        save_path_file = os.path.join(save_path, name)    # 存储图片到本地文件夹
+        cv2.imwrite(save_path_file, img)                          # 存储
 
-    # 剪切出图片
+    # 垂直拼接图片
+    def pinjie(self):
+        # 第一张底部剪切出模版图
+        # 匹配第二张图中坐标位置
+        # 第二张图剪切最顶部
+        # 第一张图 + 剪切后的第二张图合成
+        pass
 
-    # 保存到本地；返回文件地址、文件名称
 
 if __name__ == '__main__':
 
-    img_url = 'img/template/detaile_page.jpg'
+    img_url = 'img/template/detaile_page.jpg'   # 原图
 
-    template_img_url = 'img/template/shop.jpg'
+    template_img_url = 'img/template/shop.jpg'  # 模版
 
     s = Selectimg(img_url,template_img_url)
 
